854cd0f8c9ab25c84cc840a4045d6c72395ce748
au10tixmobile <94300864+au10tixmobile@users.noreply.github.com>
2024-03-05 19:04:05 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,21 +14,9 @@ import Foundation
 let package = Package(
     name: "au10tixsmartdocumentcapturekit-ios",
     products: [ 
-        .library(name: "Au10tixCore", targets: ["Au10tixCore"]),
-        .library(name: "Au10tixSourceManager", targets: ["Au10tixSourceManager"]),
         .library(name: "Au10tixSmartDocumentCaptureKit", targets: ["Au10tixSmartDocumentCaptureKit"])
     ],
     targets: [
-        .binaryTarget(
-          name: "Au10tixCore",
-          url: "https://github.com/au10tixmobile/Au10tixCore/archive/refs/tags/4.0.0.zip",
-          checksum: "ed5691a5d3bf0e519daa6c0e814c9900d77bd6db00aac68b1931afc87d6683f6"
-        ),
-        .binaryTarget(
-          name: "Au10tixSourceManager",
-          url: "https://github.com/au10tixmobile/Au10tixSourceManager/archive/refs/tags/4.0.0.zip",
-          checksum: "8bc55b8fe260a75fcd1a191e2c0f02faf1c10b805c4cbf7c00ea8eb74c66ec1f"
-        ),
         .binaryTarget(
           name: "Au10tixSmartDocumentCaptureKit",
           url: "https://github.com/au10tixmobile/Au10tixSmartDocumentCaptureKit/archive/refs/tags/4.0.0.zip",
